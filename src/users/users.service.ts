552b6294ba67e44ea50b6d66e667e024b3d4f31d
ruhan <robiuzzaman4@gmail.com>
2026-06-01 02:34:21 +0600

    refactor: simplify getUser method by removing async keyword

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { db } from '../db';
 
 @Injectable()
 export class UsersService {
-  async getUser(userId: string) {
+  getUser(userId: string) {
     // Example: query the Better Auth user table directly via Drizzle
     // Better Auth's tables are available on the db instance
     return { id: userId }; // expand this as you need
